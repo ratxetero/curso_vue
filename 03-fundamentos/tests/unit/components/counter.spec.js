ea682b92b5f0f581ded2db8d30eb0c234517a60b
Alejandro <daw01.2021@gmail.com>
2022-02-04 14:53:55 +0100

    Curso vue finalizado

---
+++ 03-fundamentos/tests/unit/components/counter.spec.js
@@ -0,0 +1,11 @@
+import {shallowMount } from '@vue/test-utils'
+import Counter from '@/components/Counter'
+
+describe ('Counter Component', () =>{
+    test('debe hacer match con el snapshot', ()=>{
+
+        const wrapper = shallowMount(Counter)
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+})
+
---
+++ 05-rutas-ciclom/src/App.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <AboutPage/>
+  </div>
+</template>
+
+<script>
+import AboutPage from './modules/pokemon/pages/AboutPage'
+export default {
+  name: 'App',
+  components: {
+  AboutPage
+    } 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
